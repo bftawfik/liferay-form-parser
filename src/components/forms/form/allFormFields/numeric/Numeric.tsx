480710924e894320d320447ab96350ae54d18cd7
Omar Farahat <omar.farahat@itworx.com>
2025-02-11 14:53:22 +0200

    Added FieldType Feature to Numeric

--- src/components/forms/form/allFormFields/numeric/Numeric.tsx
+++ src/components/forms/form/allFormFields/numeric/Numeric.tsx
@@ -1,13 +1,31 @@
 import { FieldType } from "../../../../../types/forms";
 import { FormLabelAndTooltip } from "../../HelperComponents/FormLabelAndTooltip";
 import { FormControl } from "@mui/material";
-import MuiTextField from "@mui/material/TextField";
+import { NumberField } from "@base-ui-components/react/number-field";
+import { useState } from "react";
 
 interface NumericType {
   formData: FieldType;
 }
 
 const Numeric: React.FC<NumericType> = ({ formData }) => {
+  const [value, setValue] = useState<string>(
+    formData.predefinedValue !== "null" ? String(formData.predefinedValue) : ""
+  );
+
+  const isDecimal = formData.dataType === "double";
+
+  const handleChange = (newValue: string) => {
+    if (isDecimal) {
+      if (/^\d*\.?\d*$/.test(newValue)) {
+        setValue(newValue);
+      }
+    } else {
+      if (/^\d+$/.test(newValue)) {
+        setValue(newValue);
+      }
+    }
+  };
   return (
     <FormControl fullWidth>
       <FormLabelAndTooltip
@@ -15,14 +33,21 @@ const Numeric: React.FC<NumericType> = ({ formData }) => {
         tooltip={formData.tooltip}
         showLabel={formData.showLabel}
       >
-        <MuiTextField
-          type="number"
-          placeholder={formData.placeholder}
-          defaultValue={formData.predefinedValue}
-          variant="outlined"
-          sx={{ mt: 2 }}
-          fullWidth
-        />
+        <NumberField.Root value={parseFloat(value)}>
+          <NumberField.Group>
+            <NumberField.Input
+              className="p-2 border rounded"
+              inputMode={isDecimal ? "decimal" : "numeric"}
+              step={isDecimal ? 0.01 : 1}
+              value={value}
+              onChange={(e) => handleChange(e.target.value)}
+              placeholder={formData.placeholder as string}
+              style={{
+                color: value ? "black" : "gray",
+              }}
+            />
+          </NumberField.Group>
+        </NumberField.Root>
       </FormLabelAndTooltip>
     </FormControl>
   );
